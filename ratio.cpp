26ad8110262fb616ea59f637fc60cb716c8f39fe
INNOCENT-ops806 <240418541@tut4life.ac.za>
2025-05-20 20:34:29 +0200

    Add centered welcome message and version info to editor screen

--- ratio.cpp
+++ ratio.cpp
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+#define RATIO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct abuf {
@@ -107,7 +108,27 @@ int getWindowSize(int *rows, int *cols) {
 
 void EditorDrawRows(struct abuf *ab) {
   for (int y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Ratio editor -- version %s", RATIO_VERSION);
+
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    }
+    else{
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K",3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -116,13 +137,13 @@ void EditorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
-
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[?25l",6);
   abAppend(&ab, "\x1b[H", 3);
 
   EditorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h",6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
